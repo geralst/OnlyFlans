54c321736deccfecf23c4ad1c4c6971a399db3fa
Esteban Geraldo <88014735+geralst@users.noreply.github.com>
2022-04-23 19:21:42 -0400

    Update settings.py

--- onlyflans/settings.py
+++ onlyflans/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_nn&9o#+7_3wud874@a%89nco1k!#v97@*3i(42fbn11t=hpng'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
